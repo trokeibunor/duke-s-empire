f23b5165539a0730764efc5d14b8133e044df0e9
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2023-10-16 10:27:11 +0100

    feat: hero section

---
+++ src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+    <nav>
+        <div class="nav_content">
+            <div class="nav_menu">
+                <a href="#" class="nav_item">Home</a>
+                <a href="#" class="nav_item">About Us</a>
+                <a href="#" class="nav_item">Services</a>
+                <a href="#" class="nav_item">Contact Us</a>
+            </div>
+            <p class="nav_logo">
+                Duke's Empire
+            </p>
+            <div class="social_icons">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/dukes.empire/" target="_blank">
+                            <img src="../assets/icons/facebook_icon.svg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/dukes_empire/" target="_blank">
+                            <img src="../assets/icons/instagram.svg" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/dukes_empire" target="_blank">
+                            <img src="../assets/icons/linkedIn_icon.svg" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+nav{
+    width: 100%;
+    .nav_menu {
+        display: flex;
+        gap: 8px
+    }
+    .nav_content {
+        width: 90%;
+        margin: 0.75rem auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .social_icons {
+            ul {
+                display: flex;
+                flex-direction: row;
+            }
+        }
+    }
+    .nav_logo {
+        font-family: 'Playfair Display', serif;
+        font-size: 18px;
+        font-weight: 600;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,72 @@
 <script setup>
+import NavBar from '../components/NavBar.vue';
 
 </script>
 
 <template>
   <main>
-    hello world
+    <div class="display_image">
+      <img src="../assets/images/backdrop_img.png" alt="">
+    </div>
+   <section class="hero">
+    <NavBar/>
+    <div class="hero_content">
+      <div class="text_section">
+       <div class="main_text">
+        <h1 style="font-weight: 700;">We Help you</h1>
+        <h1 style="font-weight: 300;">get to your <br> Potential</h1>
+       </div>
+       <div class="sub_text">
+        <p>
+          We are a team of professionals who are passionate about helping you get to your potential.
+          We are a team of professionals who are passionate about helping you get to your potential.
+        </p>
+       </div>
+       <button class="cta">GET STARTED</button>
+      </div>
+      <div class="image_section">
+        <img src="../assets/images/hero_img.png" alt="">
+      </div>
+    </div>
+   </section>
   </main>
 </template>
+
+<style>
+main {
+  position: relative;
+  overflow: hidden;
+  min-height: 100dvh;
+  .display_image {
+    position: absolute;
+    z-index: -1;
+    top: -20%;
+    right: -15%;
+  }
+  .hero {
+    height: 95dvh;
+    .hero_content {
+      width: 90%;
+      height: 100%;
+      margin: 0 auto;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+      gap: 1rem;
+      .image_section {
+        align-self: center;
+      }
+      .text_section {
+        display: flex;
+        flex-direction: column;
+        /* justify-content: center; */
+        /* align-items: center; */
+        gap: 1rem;
+        .sub_text {
+          width: 80%;
+        }
+      }
+    }
+  }
+}
+</style>
